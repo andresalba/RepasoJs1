24fdbe05d49ae90ae4ef6fae51fb8eb0125a0842
Andres Alba <tensinkusum@hotmail.com>
2019-12-23 18:30:40 -0500

    v1

---
+++ src/components/Holamundo.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HolaMundo = () => {
+	const Hello = 'hola mundo';
+	const isTrue = false;
+	return{
+		<div className="HolaMundo">
+			<h1>{Hello}</h1>
+			<h2>React Basico</h2>
+			<img src="https://arepa.s3.amazonaws.com/react.png" alt="React" />
+			{isTrue ? <h4>verdadero</h4> : <h5>falso</h5>}
+			{isTrue && <h4>verdadero nuevamente</h4>}
+		</div>
+	};
+};
+
+export default HolaMundo;
---
+++ src/components/Presentacional.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Presentacional = () => {<h1>hola mundo</h1>};
+
+export default Presentacional;
